c32bb84601485896077bd07f9e65dab2e6218ab1
PauloShit <paulinuquiza@gmail.com>
2025-03-14 15:59:27 +0000

    Update main.py

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ def eco():
     return render_template('eco.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=10000, debug=True)
